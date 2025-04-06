f770849a0fc9091dc32f50e3333ccf6f9593e448
Ahmadjon <@robotoprotecno2008@gmail.com>
2025-04-06 16:49:12 +0500

    edited

--- mybooks/models.py
+++ mybooks/models.py
@@ -3,10 +3,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    year = models.IntegerField(null=True, blank=True)
-    genre = models.CharField(max_length=100, default="noaniq")
-    description = models.TextField(default="Izoh mavjud emas.")
-
+    year = models.PositiveIntegerField()
+    genre = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
